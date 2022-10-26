fb82c2ec80352509ed43bf8cd367e281009e2a5e
fiorotticaio <caiofiorotti@gmail.com>
2022-10-26 10:58:47 -0300

    :lipstick: components edited

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -1,35 +1,68 @@
-import BoxContent from "../components/BoxContent";
+import NlwBoxContent from "../components/NlwBoxContent";
 import Navbar from "../components/Navbar";
 import Titulo from "../components/Titulo";
 import Ignite from "../assets/nlw_esports.png";
+import Divider from "../components/Divider";
+import YtBoxContent from "../components/YtBoxContent";
 
 export default function Content() {
     return (
-        <>
-            <div className="bg-black h-full w-screen">
-                <Navbar />
-                <section id="nlw" className="mt-12 mx-10 pt-12 overflow-hidden">
-                    <Titulo text="NLW's"/>
+        <div className="bg-black h-full w-screen">
+            <Navbar />
 
-                    <BoxContent 
-                        title="Ignite" 
-                        description="Cada vez mais tenho a sensação de incertezas e inseguranças e tento me manter firme apesar disso. Algumas coisas parecem dar certo e maioria não, tipo você."
-                        stack="Node, React, React Native"
-                        thumbnailPath='./src/assets/nlw_Esports.png'
+            <section id="nlw" className="my-12 mx-10 pt-12 overflow-hidden">
+                <Titulo text="NLW's"/>
+
+                <NlwBoxContent 
+                    title="Ignite" 
+                    description="Cada vez mais tenho a sensação de incertezas e inseguranças e tento me manter firme apesar disso. Algumas coisas parecem dar certo e maioria não, tipo você."
+                    stack="Node, React, React Native"
+                    thumbnailPath='./src/assets/nlw_Esports.png'
+                />
+
+                <NlwBoxContent 
+                    title="Together" 
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis tortor ut turpis auctor consequat. Ut vel orci in ligula hendrerit volutpat."
+                    stack="Java criminoso"
+                    thumbnailPath='./src/assets/together.jpg'
+                />
+            </section>
+
+            <Divider />
+
+            <section id="youtube" className="mt-12 mx-10 pt-12 overflow-hidden">
+                <Titulo text="Youtube"/>
+
+                <div className="flex justify-between mx-[150px]">
+                    <YtBoxContent 
+                        title="HTML, CSS e Bootstrap"
+                        canal="Micheli Brito"
+                        thumbnailPath="./src/assets/micheli.png"
+                        link="https://youtube.com/playlist?list=PL8iIphQOyG-Cv3auRYoZtbvzJJrsvdMy-"
                     />
 
-                    <BoxContent 
-                        title="Together" 
-                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis tortor ut turpis auctor consequat. Ut vel orci in ligula hendrerit volutpat."
-                        stack="Java criminoso"
-                        thumbnailPath='./src/assets/together.jpg'
+                    <YtBoxContent 
+                        title="HTML, CSS e Bootstrap"
+                        canal="Micheli Brito"
+                        thumbnailPath="./src/assets/micheli.png"
+                        link="https://youtube.com/playlist?list=PL8iIphQOyG-Cv3auRYoZtbvzJJrsvdMy-"
                     />
-                </section>
-                <section id="youtube"></section>
-                <section id="videos-proprios"></section>
-                <section id="slides"></section>
-                <section id="livros"></section>
-            </div>
-        </>
+
+                    <YtBoxContent 
+                        title="HTML, CSS e Bootstrap"
+                        canal="Micheli Brito"
+                        thumbnailPath="./src/assets/micheli.png"
+                        link="https://youtube.com/playlist?list=PL8iIphQOyG-Cv3auRYoZtbvzJJrsvdMy-"
+                    />
+                </div>
+
+
+
+            </section>
+
+            <section id="videos-proprios"></section>
+            <section id="slides"></section>
+            <section id="livros"></section>
+        </div>
     )
 }
--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -1,23 +1,52 @@
 import Titulo from "../components/Titulo";
 import playL from "../assets/play2.svg";
 import playR from "../assets/play3.svg";
-import playRect from "../assets/retanguloPlay.svg"
 
-function Play(){
+interface Props {
+
+}
+
+function Play({}: Props){
     return(
-        <>
-            <main className="h-screen w-screen bg-black flex flex-col">
-                <section className="h-[15%] flex items-center justify-center">
-                    <Titulo text="Aula 1 - Título da aula"/>
-                </section>
-                <section className="h-[60%] bg-purple-500-800 border-t-laranja border-b-laranja border-t-2 border-b-2 flex">
-                    <div className="h-full w-1/6 flex items-center justify-center"><img src={playL} alt="" /></div>
-                    <div className="h-full w-2/3 bg-gray-500 m-auto border-r-2 border-l-2 border-r-laranja border-l-laranja"></div>
-                    <div className="h-full w-1/6 flex items-center justify-center"><img src={playR} alt="" /></div>
-                </section>
-                <section className="h-1/4 bg-retanguloPlay bg-cover bg-no-repeat"></section>
-            </main>
-        </>
+        <main className="h-screen w-screen bg-black flex flex-col">
+            <section className="h-[15%] flex items-center justify-center">
+                <Titulo text="Aula 1 - Título da aula"/>
+            </section>
+            <section className="h-[60%] bg-purple-500-800 border-t-laranja border-b-laranja border-t-2 border-b-2 flex">
+                <div className="h-full w-1/6 flex items-center justify-center"><img src={playL} alt="" /></div>
+                <div className="h-full w-2/3 bg-gray-500 m-auto border-r-2 border-l-2 border-r-laranja border-l-laranja">
+                    <iframe src="https://drive.google.com/file/d/1ahb6-kAgaKrPOA6XriTspQabxAm5DSIE/view?usp=sharing" frameBorder="0"></iframe>
+                </div>
+                <div className="h-full w-1/6 flex items-center justify-center"><img src={playR} alt="" /></div>
+            </section>
+            <section className="flex itemsn-center justify-center h-1/4 bg-retanguloPlay bg-cover bg-no-repeat">
+                <div className="flex mt-20 justify-center">
+                    <div className="flex items-center justify-center">
+                        <div className="bg-black w-[180px] h-[70px] rounded flex items-center justify-center">
+                            <h2 className="font-russ text-white text-xl">Aula 1</h2>
+                        </div>
+                        <div className="w-20 h-5 bg-black bg-opacity-70"></div>
+                        <div className="bg-black w-[180px] h-[70px] rounded flex items-center justify-center">
+                            <h2 className="font-russ text-white text-xl">Aula 2</h2>
+                        </div>
+                        <div className="w-[78px] h-[19px] bg-black bg-opacity-70"></div>
+                        <div className="bg-black w-[180px] h-[70px] rounded flex items-center justify-center">
+                            <h2 className="font-russ text-white text-xl">Aula 3</h2>
+                        </div>
+                        <div className="w-[78px] h-[19px] bg-black bg-opacity-70"></div>
+                        <div className="bg-black w-[180px] h-[70px] rounded flex items-center justify-center">
+                            <h2 className="font-russ text-white text-xl">Aula 4</h2>
+                        </div>
+                        <div className="w-[78px] h-[19px] bg-black bg-opacity-70"></div>
+                        <div className="bg-black w-[180px] h-[70px] rounded flex items-center justify-center">
+                            <h2 className="font-russ text-white text-xl">Aula 5</h2>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+        </main>
     )
 }
 
