ea1e9e0d0e760187f4f05453f8e2a1a570ac60cb
viniciuscole <viniamorim87@hotmail.com>
2022-10-31 14:33:38 -0300

    :lipstick: Update play screen
    aaaaa

--- src/components/YtBoxContent.tsx
+++ src/components/YtBoxContent.tsx
@@ -12,12 +12,12 @@ function YtBoxContent({title, canal, thumbnailPath, link}: Props) {
     return (
         <div className="flex flex-col gap-8 text-center mt-20 w-1/4">
             <Titulo text={title} style={'text-rosinha text-3xl font-russ'} />
-            <div className="h-[220px]">
-                <a href={link} target="_blank">
+            <a href={link} target="_blank">
+                <div className="h-[220px]">
                     <img src="./src/assets/caixa.svg" alt="caixa onde vai a imagem" />
-                </a>
-                <img className="relative z-1 -top-[198px] left-0.5 w-[284px] h-[182px] overflow-hidden" src={thumbnailPath} alt="nlw" />
-            </div>
+                    <img className="relative z-1 -top-[198px] left-0.5 w-[284px] h-[182px] overflow-hidden" src={thumbnailPath} alt="nlw" />
+                </div>
+            </a>
             <p className="text-white font-sasita text-2xl">{canal}</p>
         </div>
     )
